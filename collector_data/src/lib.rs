0b171daa977678870beb0948e1573c8ba5f11d97
t-nil <936176+t-nil@users.noreply.github.com>
2024-12-11 16:18:57 +0100

    WIP

--- collector_data/src/lib.rs
+++ collector_data/src/lib.rs
@@ -1,24 +1,3 @@
-// could look like this:
-//
-// struct SAcct {
-//     jobid: String,
-//     jobidraw: String,
-//     jobname: String,
-//     user: String,
-
-//     elapsed: String, // TODO parse time
-//     state: String,   // TODO maybe parse enum?
-
-//     partition: String,
-//     ntasks: u32,
-//     alloccpus: u32,
-
-//     maxrss: String, // TODO parse units
-//     averss: String, // TODO parse units
-//     avecpu: String, // TODO parse time
-
-//     consumedenergy: f64,
-// }
 pub mod cpu;
 pub mod gpu;
 pub mod gpu_dep;
@@ -26,6 +5,7 @@ pub mod job;
 pub mod misc;
 pub mod monitoring_info;
 pub mod node;
+pub mod slurm;
 
 use std::path::{Path, PathBuf};
 
